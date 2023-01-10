0b474fabae31b80c4f4ee339b74001b2bb28fc32
Curtis Oyakoya <curtsoyaks@gmail.com>
2023-01-10 14:31:04 +0100

    feat: 🎸 added svg component

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,6 +1,8 @@
 import React from "react";
-import { Box, Heading, Text, Flex, HStack } from "native-base";
+import { Box, Heading, Text, Flex, Image, HStack } from "native-base";
 import Layout from "./common/Layout";
+import { Notify } from "./icon/svg";
+// import Notify from "../../assets/notify.svg"
 
 const Home = () => {
   return (
@@ -8,10 +10,14 @@ const Home = () => {
       <Box p="8" maxW="590">
         <Flex>
           <Heading>Hello Olamilekan</Heading>
+          <Notify />
+          {/* <Image src={Notify} alt="notify pic" /> */}
         </Flex>
 
         <Box mt="8">
-          <Heading fontSize="20" mb="2">Recommended for you</Heading>
+          <Heading fontSize="20" mb="2">
+            Recommended for you
+          </Heading>
           <HStack>
             <Box>
               <Box mb="2" height="200px" bg="amber.400"></Box>
